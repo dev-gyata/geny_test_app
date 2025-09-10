920a16c6298107f2b4456e6cae0a4d9a6ec9f085
gyata <felixasamoah539@gmail.com>
2025-09-10 20:24:08 +0000

    feat: Added business change notifier which uses business repository for data fetching

---
+++ lib/features/home/notifiers/business_notifier.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/foundation.dart';
+import 'package:geny_test_app/core/models/business_model.dart';
+import 'package:geny_test_app/core/models/item_fetcher_model.dart';
+import 'package:geny_test_app/core/repositories/business_repository.dart';
+
+class BusinessNotifier extends ChangeNotifier {
+  BusinessNotifier({required BusinessRepository businessRepository})
+    : _businessRepository = businessRepository;
+
+  final BusinessRepository _businessRepository;
+  ItemFetcherModel<List<BusinessModel>> _state =
+      const ItemFetcherModel.initial();
+
+  ItemFetcherModel<List<BusinessModel>> get state => _state;
+
+  Future<void> fetchBusinesses() async {
+    try {
+      _state = const ItemFetcherModel.loading();
+      final businessesResponse = await _businessRepository.getBusinesses();
+      _state = ItemFetcherModel.success(businessesResponse);
+    } catch (e) {
+      _state = ItemFetcherModel.failed(e.toString());
+    }
+  }
+}
