f7b8420d2fc8a9ae394c4fefbf5289dec9292b72
gyata <felixasamoah539@gmail.com>
2025-09-10 20:57:34 +0000

    feat: Configure dependencies during bootstrap process

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 import 'dart:developer';
 import 'package:flutter/widgets.dart';
+import 'package:geny_test_app/core/injector/di.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
+  configureDependencies();
   runApp(await builder());
 }
